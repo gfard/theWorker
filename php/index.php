b302e4c3f5f07c5168b591375f87fc8a59c26755
George Fardellas <f.tzortzis@gmail.com>
2014-01-10 11:44:47 +0000

    TESTn

--- php/index.php
+++ php/index.php
@@ -1,5 +1,4 @@
-TEST FROM CODENVY
-<?php
+TEST CODENVY<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
