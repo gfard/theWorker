cd1023204a001fdc47af79eeb7807b933346ce51
gfard <f.tzortzis@gmail.com>
2014-01-10 14:25:10 +0200

    TEST FROM HOMEPC FROM TORTOISEGIT

--- php/index.php
+++ php/index.php
@@ -1,4 +1,6 @@
-TEST CODENVY<?php
+TEST CODENVY
+TEST FROM HOMEPC-UPLOAD WITH GITHUB
+<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
